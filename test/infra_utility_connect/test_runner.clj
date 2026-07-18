1ea53870784fae349af344d1dfa7bbe515e4352e
Jun Kawasaki <jun784@gmail.com>
2026-07-18 20:57:26 +0900

    refactor: consolidate utility connector implementation

---
+++ test/infra_utility_connect/test_runner.clj
@@ -0,0 +1,16 @@
+(require '[clojure.test :as t]
+         'infra-utility-connect.methods.test-agent
+         'infra-utility-connect.cells.activation-test.test-state-machine
+         'infra-utility-connect.cells.meter-install.test-state-machine
+         'infra-utility-connect.cells.provider-approval.test-state-machine
+         'infra-utility-connect.cells.service-request.test-state-machine)
+
+(let [result
+      (t/run-tests
+       'infra-utility-connect.methods.test-agent
+       'infra-utility-connect.cells.activation-test.test-state-machine
+       'infra-utility-connect.cells.meter-install.test-state-machine
+       'infra-utility-connect.cells.provider-approval.test-state-machine
+       'infra-utility-connect.cells.service-request.test-state-machine)]
+  (when (pos? (+ (:fail result) (:error result)))
+    (System/exit 1)))
